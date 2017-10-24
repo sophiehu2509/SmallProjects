09ac0eac91ebc67262666a1342e89522afdac06b
Sophie <sophie.huye@gmail.com>
2017-10-23 21:45:37 -0400

    Bouncing Ball

--- Proj_5/js/script.js
+++ Proj_5/js/script.js
@@ -1,45 +1,37 @@
-function randomColor(){
-  var r=Math.floor(Math.random()*255);
-  var g=Math.floor(Math.random()*255);
-  var b=Math.floor(Math.random()*255);
-  var rgb='rgb('+r+','+g+','+b+')';
-  //$(this).css({background-color: rgb});
-  $(this).css('background-color', rgb);
-  $(this).css('z-index', "1000");
-  $(this).css('box-shadow', "0 0 8px white");
-}
+$(document).ready(function(){
+  var con = ["Good Job!","Nice Job!"];
 
-function end(){
-  //$(this).css({background-color: rgb});
+  var levels = 1;
+  $("#ball").on("click",function(){
 
-  $(this).css('z-index', "1");
-  $(this).css('box-shadow', "none");
-}
+    if (levels < 3){
+      speedChange(this);
+      showMessage();
+    }else{
+      $("#congra").text("You are winner!");
+      $("#ball").hide();
+      $("#level").hide();
+      $(".message").show();
+    }
+  });
 
-var color = document.getElementsByClassName('box');
-for(var i = 0; i<color.length; i ++){
-  color[i].addEventListener('mouseover', randomColor, false);
-}
-for(var i = 0; i<color.length; i ++){
-  color[i].addEventListener('mouseleave', end, false);
-}
+  function speedChange (elm){
+    var speed = $(elm).css('animation-duration');
+    var newSpeed = (speed.split('s'))[0] -1;
+    $(elm).css('animation-duration', newSpeed + 's');
+  }
 
+  function showMessage(){
 
+    levels += 1;
+    $("#l").text(levels);
 
-/*$(document).ready(function () {
+    $("#ball").hide();
+    $(".message").show();
 
-	function randomColor() {
-		return '#' + Math.random().toString(16).slice(2, 8);
-	};
-
-	$(".box").on("mouseenter", function () {
-		$(this).css('background-color', randomColor());
-		$(this).css('box-shadow', "0 0 8px white");
-		$(this).css('z-index', "100000");
-	});
-
-	$(".box").on("mouseleave", function () {
-		$(this).css('box-shadow', "none");
-		$(this).css('z-index', "1");
-	});
-});*/
+    setTimeout(function(){
+      $("#ball").show();
+      $(".message").hide();
+    }, 1000);
+  }
+});
